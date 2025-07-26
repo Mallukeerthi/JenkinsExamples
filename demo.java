e42d54639ff05f2a829e4970d4b8c308f71ca0d7
Mallukeerthi <117076788+Mallukeerthi@users.noreply.github.com>
2025-07-26 10:52:34 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,13 @@
 class demo{
 public static void main(String args[]){
-System.out.println("hello");
+  int sum=0;
+  int avg=0;
+  for(int i=1;i<=10;i++0
+    {
+    sum=sum+i;
+  }
+System.out.println("sum"+sum);
+avg=sum/10;
+System.out.println("avg"+avg);
 }
 }
